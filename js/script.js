13202d1c1756cd197355782d8898e4edc9b7a005
rath-prak <rathanap@gmail.com>
2016-03-03 16:00:22 +1300

    update JS and CSS

--- js/script.js
+++ js/script.js
@@ -1,32 +1,62 @@
-var number = "";
+var displayBox = document.getElementById("display_answer");
 
-function calculator(digit){
+function buttonValue(keyValue){
+	displayBox.value += keyValue;
 
-	//answer
-	
-	if(digit === "="){
-		console.log(eval(number));
-		$("#answer").val(eval(number));
-	}
-
-	else if(digit === "C"){
-		location.reload();
-	}
-	else{
-		number += digit;
-		$("#answer").val(number);
+	if (keyValue == "CLEAR"){
+		displayBox.value = '';
 	}
 
 }
 
-// "5 * 6" = 
+function answer (){
+	var sum = displayBox.value;
+	sum = eval(sum);
+	displayBox.value = sum;
+
+}
 
 
-	// else if(digit === "+"){
-	// 	number += digit;
-	// }
-
-// $(document).ready(function(){
 
 
-// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 1. var displayBox = document.getElementById("display_answer");
+   - This basically declares a variable where we can access the HTML elements, grab the values from the 
+   buttons and stores them in the variable. 
+   
+   2. Create a function that takes the values from the button and displays it when a button is clicked. 
+
+   displayBox.value = key;
+	
+	this function basically takes value of button and returns it to buttonValue, then appends it to 
+	the displayBox
+
+	The 'value' property assigns a value of the variable or attribute. Here the value of displaybox is equals
+	'keyValue'. 
+
+	The '+=' is used to add new values to the string without deleting the previous values. i.e. add values 
+	continuouly. 
+
+	3. When the 'clear' button is clicked, the textbox should reset. This is done by assigning the value of 
+	the 'displayBox' to an empty string. e.g. displayBox.value = '';
+
+	4. Declare the 'answer()' function, use 'eval()' to do the maths. 
+
+
+*/
+
+
